1df2d3a76a82af326c784e3aab903d9e5c810b50
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2018-04-18 12:03:11 +0100

    Added Chalkdust pattern

--- patterns/__init__.py
+++ patterns/__init__.py
@@ -1,5 +1,7 @@
 from .gun import Glider as _Glider
 from .gun import GosperGliderGun as _GosperGliderGun
+from .chalkdust import Chalkdust as _Chalkdust
 
 glider = _Glider()
 gun = _GosperGliderGun()
+chalkdust = _Chalkdust()
